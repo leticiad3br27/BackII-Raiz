20d9ffc0f3c473cdcc59c112ac9d05ece1151458
Leticia2711 <leticia27belo11@gmail.com>
2025-01-30 11:39:00 -0400

    editei

--- API/index.js
+++ API/index.js
@@ -1,15 +1,43 @@
 import express  from "express";
 import pool from "./servico/conexao.js";
+import { retornaCampeonatos, retornaCampeonatosID } from "./servico/retornaCampeonatos_servico.js";
 
 
 
 
 const app =express();
 
+app.get('/campeonatos/:id',async (req,res)=>{
+    const id =parseInt(req.params.id);
+    const campeonato=await retornaCampeonatosID(id);
+    if (campeonato.length > 0){
+        res.json(campeonato);}
+        else{
+            res.status(404).json({mensagem:"nenhum campeonato encontrado"});
+
+        }
+    });
+
+    app.get('/campeonatos', async (req, res) => {
+        let campeonatos;
+        const ano= req.query.ano;
+        if (typeof ano === 'undefined') {
+        campeonatos = await retornaCampeonatos();
+        } 
+        else {
+        }
+        campeonatos = await retornaCampeonatosAno (parseInt(ano));
+        if (campeonatos.length > 0) {
+        res.json(campeonatos);
+        } else {
+       
+        res.status(404).json({ mensagem: "Nenhum campeonato encontrado" });
+        }
+    })
 app.listen(9000, async () => {
     const data= new Date ();
     console.log('Servidor iniciado em:'+data);
-    const conexao  = await pool.getConnection();
-    console.log(conexao.threadId);
-    conexao.release();
+    // const conexao  = await pool.getConnection();
+    // console.log(conexao.threadId);
+    // conexao.release();
 })
---
+++ API/servico/retornaCampeonatos_servico.js
@@ -0,0 +1,26 @@
+import pool from "./conexao.js";
+
+export async function retornaCampeonatos(){
+    const conexao= await pool.getConnection();
+    const campeonatos_tb= await conexao.query('SELECT id,campeao,vice,ano FROM campeonatos');
+    const campeonatos= campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
+
+export async function retornaCampeonatosID(){
+    const conexao= await pool.getConnection();
+    const campeonatos_tb= await conexao.query('SELECT id,campeao,vice,ano FROM campeonatos WHARE id'+id);
+    const campeonatos= campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+}
+
+export async function retornaCampeonatosAno (ano) {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query(
+    'SELECT id, campeao, vice, ano FROM campeonatos WHERE ano =' +ano);
+    const campeonatos = campeonatos_tb[0];
+    conexao.release();
+    return campeonatos;
+    }
